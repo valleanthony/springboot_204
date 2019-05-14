90c15f82d3edbf36cfabc06a195a359e445f5368
Anthony Valle <valleganthony@gmail.com>
2019-05-14 15:29:30 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_204/Tvshow.java
@@ -0,0 +1,53 @@
+package com.example.springboot_204;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Tvshow {
+    @NotNull
+    @Min(1)
+    private long id;
+
+    @NotNull
+    @Size(min = 3, max = 10)
+    private String type;
+
+    @NotNull
+    @Size(min = 10, max = 30)
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Tvshow() {
+    }
+
+    public Tvshow(@NotNull @Min(1) long id, @NotNull @Size(min = 3, max = 10) String type, @NotNull @Size(min = 10, max = 30) String description) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+    }
+}
+
